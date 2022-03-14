fe93e33e5d564319a64e464f94897a99d1488b0a
JK Gunnink <jgunnink@gmail.com>
2022-03-14 16:55:09 +0800

    reduce some duplication

--- api/weatherapi_test.go
+++ api/weatherapi_test.go
@@ -16,10 +16,11 @@ func init() {
 }
 
 func TestGetWeather(t *testing.T) {
+	Client = &mocks.MockClient{}
+	api_response := `{"request":{"type":"City","query":"Sydney, Australia","language":"en","unit":"m"},"location":{"name":"Sydney","country":"Australia","region":"New South Wales","lat":"-33.883","lon":"151.217","timezone_id":"Australia\/Sydney","localtime":"2022-03-14 17:20","localtime_epoch":1647278400,"utc_offset":"11.0"},"current":{"observation_time":"06:20 AM","temperature":22,"weather_code":116,"weather_icons":["https:\/\/assets.weatherstack.com\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png"],"weather_descriptions":["Partly cloudy"],"wind_speed":15,"wind_degree":170,"wind_dir":"S","pressure":1020,"precip":0,"humidity":78,"cloudcover":75,"feelslike":25,"uv_index":6,"visibility":10,"is_day":"yes"}}`
+	r := ioutil.NopCloser(bytes.NewReader([]byte(api_response)))
+
 	t.Run("when no query string is provided, it defaults to Sydney", func(t *testing.T) {
-		Client = &mocks.MockClient{}
-		api_response := `{"request":{"type":"City","query":"Sydney, Australia","language":"en","unit":"m"},"location":{"name":"Sydney","country":"Australia","region":"New South Wales","lat":"-33.883","lon":"151.217","timezone_id":"Australia\/Sydney","localtime":"2022-03-14 17:20","localtime_epoch":1647278400,"utc_offset":"11.0"},"current":{"observation_time":"06:20 AM","temperature":22,"weather_code":116,"weather_icons":["https:\/\/assets.weatherstack.com\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png"],"weather_descriptions":["Partly cloudy"],"wind_speed":15,"wind_degree":170,"wind_dir":"S","pressure":1020,"precip":0,"humidity":78,"cloudcover":75,"feelslike":25,"uv_index":6,"visibility":10,"is_day":"yes"}}`
-		r := ioutil.NopCloser(bytes.NewReader([]byte(api_response)))
 		mocks.GetDoFunc = func(*http.Request) (*http.Response, error) {
 			return &http.Response{
 				StatusCode: 200,
@@ -38,8 +39,6 @@ func TestGetWeather(t *testing.T) {
 	})
 
 	t.Run("the response includes just the wind speed and temperature", func(t *testing.T) {
-		Client = &mocks.MockClient{}
-		api_response := `{"request":{"type":"City","query":"Sydney, Australia","language":"en","unit":"m"},"location":{"name":"Sydney","country":"Australia","region":"New South Wales","lat":"-33.883","lon":"151.217","timezone_id":"Australia\/Sydney","localtime":"2022-03-14 17:20","localtime_epoch":1647278400,"utc_offset":"11.0"},"current":{"observation_time":"06:20 AM","temperature":22,"weather_code":116,"weather_icons":["https:\/\/assets.weatherstack.com\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png"],"weather_descriptions":["Partly cloudy"],"wind_speed":15,"wind_degree":170,"wind_dir":"S","pressure":1020,"precip":0,"humidity":78,"cloudcover":75,"feelslike":25,"uv_index":6,"visibility":10,"is_day":"yes"}}`
 		r := ioutil.NopCloser(bytes.NewReader([]byte(api_response)))
 		mocks.GetDoFunc = func(*http.Request) (*http.Response, error) {
 			return &http.Response{
@@ -61,8 +60,6 @@ func TestGetWeather(t *testing.T) {
 	})
 
 	t.Run("When there is a query string provided, it uses the query", func(t *testing.T) {
-		Client = &mocks.MockClient{}
-		api_response := `{"request":{"type":"City","query":"Sydney, Australia","language":"en","unit":"m"},"location":{"name":"Sydney","country":"Australia","region":"New South Wales","lat":"-33.883","lon":"151.217","timezone_id":"Australia\/Sydney","localtime":"2022-03-14 17:20","localtime_epoch":1647278400,"utc_offset":"11.0"},"current":{"observation_time":"06:20 AM","temperature":22,"weather_code":116,"weather_icons":["https:\/\/assets.weatherstack.com\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png"],"weather_descriptions":["Partly cloudy"],"wind_speed":15,"wind_degree":170,"wind_dir":"S","pressure":1020,"precip":0,"humidity":78,"cloudcover":75,"feelslike":25,"uv_index":6,"visibility":10,"is_day":"yes"}}`
 		r := ioutil.NopCloser(bytes.NewReader([]byte(api_response)))
 		mocks.GetDoFunc = func(*http.Request) (*http.Response, error) {
 			return &http.Response{
@@ -82,8 +79,6 @@ func TestGetWeather(t *testing.T) {
 	})
 
 	t.Run("When an unknown query string is provided, it defaults to Sydney", func(t *testing.T) {
-		Client = &mocks.MockClient{}
-		api_response := `{"request":{"type":"City","query":"Sydney, Australia","language":"en","unit":"m"},"location":{"name":"Sydney","country":"Australia","region":"New South Wales","lat":"-33.883","lon":"151.217","timezone_id":"Australia\/Sydney","localtime":"2022-03-14 17:20","localtime_epoch":1647278400,"utc_offset":"11.0"},"current":{"observation_time":"06:20 AM","temperature":22,"weather_code":116,"weather_icons":["https:\/\/assets.weatherstack.com\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png"],"weather_descriptions":["Partly cloudy"],"wind_speed":15,"wind_degree":170,"wind_dir":"S","pressure":1020,"precip":0,"humidity":78,"cloudcover":75,"feelslike":25,"uv_index":6,"visibility":10,"is_day":"yes"}}`
 		r := ioutil.NopCloser(bytes.NewReader([]byte(api_response)))
 		mocks.GetDoFunc = func(*http.Request) (*http.Response, error) {
 			return &http.Response{
